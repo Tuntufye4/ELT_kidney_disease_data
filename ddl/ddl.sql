58af5366b024152ea0cd45158c3271649db24019
Tuntufye4 <tuntumwanyongo@gmail.com>
2023-10-02 14:40:05 +0200

    added ddl script

---
+++ ddl/ddl.sql
@@ -0,0 +1,32 @@
+create schema kidney;
+
+create table kidney.kidney_disease (
+      id int,
+	  age int,
+	  bp int,
+	  sg  float,
+	  albumin int,
+	  sugar int,
+	  red_blood_cell_count varchar(20),
+	  pus_cells varchar(20),
+	  pcs varchar(20),
+	  ba varchar(20),
+	  bgr int,
+	  bu int,
+	  sc float,
+	  sodium int,
+	  potassium float,
+	  Hemoglobin float,
+	  pcv int,
+	  wbcc int,
+	  rbcc int,
+	  htn varchar(8),
+	  dm varchar(8),
+	  cad varchar(8),
+      appetite varchar(20),
+	  pe varchar(10),
+	  anemia varchar(20),
+	  classification varchar(10)
+
+);
+
