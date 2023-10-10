7c2c7a0de343c717e6979bd449bc57bfefedd331
Tuntufye Mwanyongo <tuntumwanyongo@gmail.com>
2023-10-10 09:09:01 +0200

    Update kidney_aggregations.sql

--- kidney_disease_class/models/kidney_aggregations/kidney_aggregations.sql
+++ kidney_disease_class/models/kidney_aggregations/kidney_aggregations.sql
@@ -5,19 +5,21 @@ with ref_kidney_restructure as (
 
 final as (
     select
-        classification,
-        max(age) as max_age,
-        min(age) as min_age,
-        avg(serum_creatinine) as avg_serum_creatinine,
-        count(*) as total_patients,
-        case
-            when hypertension = 'yes' and diabetes_mellitus = 'yes' then 'Both HTN and DM'
-            when hypertension = 'yes' then 'HTN Only'
-            when diabetes_mellitus = 'yes' then 'DM Only'
-            else 'Neither HTN nor DM'
-        end as condition_group
-    from ref_kidney_restructure
-    group by classification, hypertension, diabetes_mellitus
+        max(rkr.age) as max_age,
+        min(rkr.age) as min_age,
+        max(rkr.sugar) as max_sugar,
+        min(rkr.sugar) as min_sugar,
+        max(rkr.Blood_Glucose_Random) as max_blood_glucose_random,
+        min(rkr.Blood_Glucose_Random) as min_blood_glucose_random,
+        max(rkr.Blood_Urea) as max_blood_urea,
+        min(rkr.Blood_Urea) as min_blood_urea,
+        max(rkr.Packed_Cell_Volume) as max_packed_cell_volume,
+        min(rkr.Packed_Cell_Volume) as min_packed_cell_volume,
+        max(rkr.bp) as max_bp,
+        min(rkr.bp) as min_bp,
+        count(*) as total_patients
+    
+    from ref_kidney_restructure rkr
 )
 
 select * from final;
